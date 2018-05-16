27b478ccb355c8073d0128eec53e63629eaae07d
corjuela <corjuela@lannex.com.au>
2018-05-16 17:09:51 +1000

    Test renaming theme
    This is a test renaming an existing theme

---
+++ theme_lannex_experts/__manifest__.py
@@ -0,0 +1,27 @@
+{
+    'name': 'Lannex Experts Theme',
+    'description': 'Lannex Experts Business Theme',
+    'category': 'Theme/Corporate',
+    'sequence': 210,
+    'version': '1.0',
+    'author': 'Lannex',
+    'depends': ['theme_lannex_loftspace', 'website_animate', 'snippet_google_map'],
+    'data': [
+        'views/assets.xml',
+        'views/images.xml',
+        'views/preset.xml',
+        'views/layout.xml',
+        'views/snippets.xml',
+    ],
+    'demo': [
+        'demo/demo.xml',
+        'demo/blocks.xml',
+    ],
+    'images': [
+        'static/description/odoo_experts_description.png',
+        'static/description/odoo_experts_screenshot.jpeg',
+    ],
+    'price': 4,
+    'currency': 'EUR',
+    'live_test_url': 'https://theme-odoo-experts.odoo.com',
+}
